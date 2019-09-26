81cdd49ff5e58e363411815ea59fd73cfe676876
virustotalop <virustotalop@gmail.com>
2019-09-26 06:05:48 -0700

    Expose PermissionManager from Foundry

--- src/main/java/com/clubobsidian/foundry/Foundry.java
+++ src/main/java/com/clubobsidian/foundry/Foundry.java
@@ -2,19 +2,29 @@ package com.clubobsidian.foundry;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.clubobsidian.foundry.permission.PermissionManager;
+
 public class Foundry extends JavaPlugin {
 
 	private static Foundry instance;
+	
+	private PermissionManager permissionManager;
 	
 	@Override
 	public void onEnable()
 	{
 		instance = this;
+		this.permissionManager = new PermissionManager();
 		this.getLogger().info("Foundry is now enabled and waiting for registration!");
 	}
 	
 	public static Foundry get()
 	{
 		return instance;
+	}
+	
+	public PermissionManager getPermissionManager()
+	{
+		return this.permissionManager;
 	}
 }
